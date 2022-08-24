620173e2be4a46b49e01c7f536735d4394c705ec
Carlos Eduardo de Araujo <carlos.araujo@youse.com.br>
2022-08-24 16:10:41 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { Container } from 'react-bootstrap';
+import Image from 'react-bootstrap/Image'
+import Faq from "./components/Faq";
+import ReactFullpage from '@fullpage/react-fullpage-umd'; // will return static version on server and "live" version on client
+import { motion, useScroll } from "framer-motion"
 
 function App() {
   return (
     <div className="App">
+      <Container>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Image src="https://gen2.focolare.org/wp-content/uploads/2022/02/K.Venturilio@CSCAudiovisivi_20170706_GF_Day1_CrowdShots-12filtrato.png" thumbnail responsive />
+
+        </header>
+
+        <section>
+          < Faq />
+        </section>
+
+      </Container>
     </div>
   );
 }
