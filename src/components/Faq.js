e78da304bca976e1501b59c207ab906ba97f7e88
Carlos Eduardo de Araujo <carlos.araujo@youse.com.br>
2022-09-13 12:15:30 -0300

    add new infos

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -18,9 +18,9 @@ export const Faq = () => {
           <Accordion.Item eventKey="1">
             <Accordion.Header>Qual será o estilo da acomodação no Hotel Rio das Garças?</Accordion.Header>
             <Accordion.Body>
-              <p>Para começo de conversa, lembramos que o Hotel Rio das Garças não tem a estrutura de um Centro Mariápolis. Trata-se de um hotel que será adaptado às necessidades do congresso Gen. A escolha do hotel se baseou em várias pesquisas e foi a proposta mais econômica que encontramos para hospedagem e alimentação, além de estar localizado na região nordeste, que abriga a maior parte dos e das Gen do Brasil.</p>
+              <p>Lembramos que o Hotel Rio das Garças não tem a estrutura de um Centro Mariápolis. Trata-se de um hotel que será adaptado às necessidades do congresso Gen. A escolha do hotel se baseou em várias pesquisas e foi a proposta mais econômica que encontramos para hospedagem e alimentação, além de estar localizado na região nordeste, que abriga a maior parte dos e das Gen do Brasil.</p>
 
-              <p>Com relação aos quartos, o Hotel dispõe de 26 bangalôs (quartos), nos quais serão distribuídos em torno de 12 pessoas (por bangalô), em beliches. Para cada bangalô teremos um banheiro. Sendo assim, contamos com a iniciativa e caridade de cada um em organizar as filas de banheiro e banhos.</p>
+              <p>Com relação aos quartos, o Hotel dispõe de 26 bangalôs (quartos), nos quais serão distribuídos em torno de 12 pessoas (por bangalô), em beliches. Para cada bangalô teremos um banheiro. Sendo assim, contamos com a iniciativa e caridade de cada um(a) em organizar as filas de banheiro e banhos.</p>
 
               <p>Para o programa, teremos um espaço ao ar livre, com cobertura por tendas, para a realização dos temas e painéis, além de muitas opções para os momentos de verde.</p>
             </Accordion.Body>
@@ -38,6 +38,7 @@ export const Faq = () => {
                 Quando for solicitado o comprovante de pagamento, em caso de substituição, o/a gen pode adicionar qualquer documento ou foto apenas para finalizar a inscrição.
               </p>
             </Accordion.Body>
+
           </Accordion.Item>
           <Accordion.Item eventKey="3">
             <Accordion.Header>Qual o horário de Check In e Check Out do hotel?</Accordion.Header>
@@ -45,6 +46,26 @@ export const Faq = () => {
               Seguindo as regras do Hotel Rio das Garças e do pacote contratado pelo Movimento dos Focolares, o horário de check in (entrada no hotel) será a partir das 14h00 do dia 11/11/2022 (sexta-feira) e de check out (saída) até as 14h00 do dia 15/11/2022.
             </Accordion.Body>
           </Accordion.Item>
+
+
+          <Accordion.Item eventKey="4">
+            <Accordion.Header>Se eu chegar dias antes ou quiser ficar dias após o congresso no Hotel Rio das Garças, é possível ficar hospedado?</Accordion.Header>
+            <Accordion.Body>
+              Sim. Para essa situação, foi proposto pelo Resort a estadia no Bangalô Luxo à partir de 4 pessoas, incluindo café da manhã e toda a estrutura de lazer do resort, no valor de R$80,00 (diária por pessoa). Com validade de 05 à 11.11.22 (pré congresso) e 15 à 18.11.22 (pós congresso)<br></br>
+
+              Condições Gerais:<br></br>
+              -Check in: a partir das 14h<br></br>
+              -Check out: às 12h<br></br>
+              -Obrigatório apresentação de documento de identidade com foto na recepção<br></br>
+              -Para cancelamento de reserva ou qualquer alteração, deverá comunicar em até 10 dias antes do check in<br></br>
+              -Pagamento via pix ou transferência bancária.<br></br>
+
+              As reservas dos dias extras devem ser feitas exclusivamente com o hotel.
+              No ato da reserva, é necessário que você informe que é participante do Movimento dos Focolares através do contato: Kelly (resort) - 84 81292505
+
+            </Accordion.Body>
+          </Accordion.Item>
+
           <Accordion.Item eventKey="5">
             <Accordion.Header>Chego na rodoviária de natal, tenho direito ao translado?</Accordion.Header>
             <Accordion.Body>
